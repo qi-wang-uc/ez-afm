698698325eb638397d9b787616b61ecdc4f820a4
Qi Wang <wangqi1900@gmail.com>
2019-05-23 04:07:38 -0400

    Update coor.cpp

--- main-prog/src/coor.cpp
+++ main-prog/src/coor.cpp
@@ -14,91 +14,87 @@
          2. Make the coding easier.
    Cons: It assumes there are no comments in the coordinates.*/
 bool CorData::read_cor(const std::string &inp_name) {
-	std::ifstream inp_file(inp_name);
-	this->_sizeof_coor = 0;
-	if(!inp_file.is_open()) {
-		std::cout << "ERROR> Could not open file: " 
-		          << inp_name 
-				  << std::endl;
-		return false;
+    std::ifstream inp_file(inp_name);
+    this->_sizeof_coor = 0;
+    if(!inp_file.is_open()) {
+        std::cout << "ERROR> Could not open file: " << inp_name  << std::endl;
+        return false;
 	} else {
-		std::cout << "ReadCOR> Reading coordinate file from: " 
-		          << inp_name 
-				  << std::endl; 
+        std::cout << "ReadCOR> Reading coordinate file from: " << inp_name  << std::endl;
 	}
-	std::string each_line;
-	std::stringstream each_stream;
-	/* For temporary variable holders */
-	std::string tmp_etc;  // direct all unwanted string streams here.
-	double tmp_xcoor = 0.0;
-	double tmp_ycoor = 0.0;
-	double tmp_zcoor = 0.0;
-	while(std::getline(inp_file, each_line)) {
-		if(each_line.empty()) continue;
+    std::string each_line;
+    std::stringstream each_stream;
+    /* For temporary variable holders */
+    std::string tmp_etc;  // direct all unwanted string streams here.
+    double tmp_xcoor = 0.0;
+    double tmp_ycoor = 0.0;
+    double tmp_zcoor = 0.0;
+    while(std::getline(inp_file, each_line)) {
+        if(each_line.empty()) continue;
 		/* The title may be printed out but not the comments.*/
-		if(each_line.substr(0,1).compare("!")==0) continue;
-		if(each_line.substr(0,1).compare("*")==0) {
-			std::cout << each_line << std::endl;
-			continue;
-		}
-		each_stream.str(each_line);
-		/* The NATOM line: Here we assume the single word line is the NATOM line, 
-		   but this does not work with any space separated comments. */
-		if(1==n_of_words(each_line)) {
-			each_stream >> this->_sizeof_coor;
-		} else {
-			each_stream >> tmp_etc >> tmp_etc >> tmp_etc >> tmp_etc
-					>> tmp_xcoor >> tmp_ycoor >> tmp_zcoor;
-			this->_xcoor.push_back(tmp_xcoor);
-			this->_ycoor.push_back(tmp_ycoor);
-			this->_zcoor.push_back(tmp_zcoor);
-		}
-		each_stream.clear();
-	}
-	inp_file.close();
-	std::cout << "ReadCOR> After reading, " 
-	          << encap(this->_sizeof_coor)
+        if(each_line.substr(0,1).compare("!")==0) continue;
+        if(each_line.substr(0,1).compare("*")==0) {
+            std::cout << each_line << std::endl;
+            continue;
+        }
+        each_stream.str(each_line);
+        /* The NATOM line: Here we assume the single word line is the NATOM line, 
+           but this does not work with any space separated comments. */
+        if(1==n_of_words(each_line)) {
+            each_stream >> this->_sizeof_coor;
+        } else {
+            each_stream >> tmp_etc >> tmp_etc >> tmp_etc >> tmp_etc
+                        >> tmp_xcoor >> tmp_ycoor >> tmp_zcoor;
+            this->_xcoor.push_back(tmp_xcoor);
+            this->_ycoor.push_back(tmp_ycoor);
+            this->_zcoor.push_back(tmp_zcoor);
+        }
+        each_stream.clear();
+    }
+    inp_file.close();
+    std::cout << "ReadCOR> After reading, " 
+              << encap(this->_sizeof_coor)
               << " atoms found in coordinate file." 
-			  << std::endl;
-	return true;
+              << std::endl;
+    return true;
 }
 
 void CorData::move_cor(Vec3d& dr, const Int& atomid, bool is_movable) {
-	if (is_movable) {
-		this->_xcoor.at(atomid) += dr.x;
-		this->_ycoor.at(atomid) += dr.y;
-		this->_zcoor.at(atomid) += dr.z;
-	}
+    if (is_movable) {
+        this->_xcoor.at(atomid) += dr.x;
+        this->_ycoor.at(atomid) += dr.y;
+        this->_zcoor.at(atomid) += dr.z;
+    }
 }
 
 float* CorData::px(void) {
-	return this->_xcoor.data();
+    return this->_xcoor.data();
 }
 
 float* CorData::py(void) {
-	return this->_ycoor.data();
+    return this->_ycoor.data();
 }
 
 float* CorData::pz(void) {
-	return this->_zcoor.data();
+    return this->_zcoor.data();
 }
 
 const Vec3d CorData::get_atom_coor(const Int& atomid) const {
-	return Vec3d(static_cast<Real>(this->_xcoor.at(atomid)),
+    return Vec3d(static_cast<Real>(this->_xcoor.at(atomid)),
                  static_cast<Real>(this->_ycoor.at(atomid)),
                  static_cast<Real>(this->_zcoor.at(atomid)));
 }
 
 Int CorData::get_coorsize() const {
-	return this->_sizeof_coor;
+    return this->_sizeof_coor;
 }
 
 void CorData::print(void) const {
-	for(Int i=0; i<this->_sizeof_coor; ++i) {
-		std::cout << std::setw(8)  << i+1
-				  << std::setw(12) << this->_xcoor.at(i)
-				  << std::setw(12) << this->_ycoor.at(i)
-				  << std::setw(12) << this->_zcoor.at(i)
-				  << std::endl;
-	}
+    for(Int i=0; i<this->_sizeof_coor; ++i) {
+        std::cout << std::setw(8)  << i+1
+                  << std::setw(12) << this->_xcoor.at(i)
+                  << std::setw(12) << this->_ycoor.at(i)
+                  << std::setw(12) << this->_zcoor.at(i)
+                  << std::endl;
+    }
 }
