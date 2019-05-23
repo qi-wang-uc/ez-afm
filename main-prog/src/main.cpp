86e19b36bb04a4ce1b77e06520624dc23286a2aa
Qi Wang <wangqi1900@gmail.com>
2019-05-23 04:12:03 -0400

    Update main.cpp

--- main-prog/src/main.cpp
+++ main-prog/src/main.cpp
@@ -3,29 +3,22 @@
 #include "../include/config.hpp"
 #include "../include/util.hpp"
 
-
 int main(int argc, char* argv[]) {
-	std::ios_base::sync_with_stdio(false);
-	std::cout << std::endl
-			  << "EZAFM> Easy Atomic Force Miscroscopy Simulations (v2.0)" 
-	          << std::endl;
-	if(argc < 2) return 1;
+    std::ios_base::sync_with_stdio(false);
+    std::cout << "\nEZAFM> Easy Atomic Force Miscroscopy Simulations (v2.0)\n";
+    if(argc < 2) return 1;
 	
 	/* Read configure file. */
-	std::string config_name = argv[1];
-	Config config;
-	if(!config.read_config(config_name)) return 1;
+    std::string config_name = argv[1];
+    Config config;
+    if(!config.read_config(config_name)) return 1;
 
-	/* Execute configure file. */
-	auto tick = TimeStamp::now();
-	if(!config.exec_config()) 
-		return 1;
-	else
-		std::cout << std::endl 
-                  << "EZAFM> Normal termination of program." 
-				  << std::endl;
-	auto tock = TimeStamp::now();
-	time_elapsed(tick, tock);
+    /* Execute configure file. */
+    auto tick = TimeStamp::now();
+    if(!config.exec_config()) return 1;
+    std::cout << "\nEZAFM> Normal termination of program.\n";
+    auto tock = TimeStamp::now();
+    time_elapsed(tick, tock);
 
-	return 0;
+    return 0;
 }
